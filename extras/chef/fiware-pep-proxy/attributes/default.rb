ee626a2af013cf6b9bcacb0995d4ff8337fffa32
aalonsog <aalonsog@dit.upm.es>
2015-09-24 12:20:47 +0200

    Refactored chef recipes

--- extras/chef/fiware-pep-proxy/attributes/default.rb
+++
@@ -1,7 +0,0 @@
-# This is a Chef attributes file. It can be used to specify default and override
-# attributes to be applied to nodes that run this cookbook.
-
-# Set a default name
-default[:appdir] = "/opt/fiware-chef-proxy"
-
-# For further information, see the Chef documentation (http://docs.getchef.com/essentials_cookbook_attribute_files.html).
