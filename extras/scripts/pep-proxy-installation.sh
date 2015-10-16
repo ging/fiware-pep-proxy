29788f4e9bcf844ee8d76a03b458b3999e1d30fb
aalonsog <aalonsog@dit.upm.es>
2015-10-16 13:34:55 +0200

    Added upstart conf to scritps

--- extras/scripts/pep-proxy-installation.sh
+++ extras/scripts/pep-proxy-installation.sh
@@ -5,7 +5,7 @@ sudo apt-get update && \
 	sudo apt-get install make g++ software-properties-common python-software-properties -y && \
 	sudo add-apt-repository ppa:chris-lea/node.js -y && \
 	sudo apt-get update && \
-	sudo apt-get install nodejs git -y && \
+	sudo apt-get install nodejs git -y
 
 # Download latest version of the code and install npm dependencies
 git clone --branch 4.4.1 https://github.com/ging/fiware-pep-proxy.git && \
