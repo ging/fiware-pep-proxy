a3533a7d88c9b276f6d82942cee1feb65cfc7aaa
aalonsog <aalonsog@dit.upm.es>
2015-10-16 13:30:46 +0200

    Added upstart conf to scritps

--- extras/scripts/pep-proxy-installation.sh
+++ extras/scripts/pep-proxy-installation.sh
@@ -8,13 +8,12 @@ sudo apt-get update && \
 	sudo apt-get install nodejs git -y && \
 
 # Download latest version of the code and install npm dependencies
-git clone --branch 4.4 https://github.com/ging/fiware-pep-proxy.git && \
+git clone --branch 4.4.1 https://github.com/ging/fiware-pep-proxy.git && \
 	cd fiware-pep-proxy && \
 	npm install
-
-sudo npm install forever -g
 
 # config.js should be configured when the instance is up and running
 cp config.js.template config.js
 
-sudo forever start server.js
+# create upstart task
+sudo cp extras/scripts/pep-proxy.conf /etc/init/pep-proxy.conf
