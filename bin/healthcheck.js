ae0119ba1e5f87872e8cf97623e2ca7e5fba04bb
Jason Fox <jason.fox@fiware.org>
2021-08-09 08:40:49 +0200

    - Format Codebase - Add copyrights - Replace in-memory object with node-cache - Add Got, remove xmlhttptrequest - promisify requests - Reduce cyclometric complexity

--- bin/healthcheck.js
+++ bin/healthcheck.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+/*
+ * Copyright 2021 -  Universidad Politécnica de Madrid.
+ *
+ * This file is part of PEP-Proxy
+ *
+ */
+
 const http = require('http');
 const config = require('../config');
 const http_code = process.env.HEALTHCHECK_CODE || 200;
@@ -8,19 +15,17 @@ function to_array(env, default_value) {
   return env !== undefined ? env.split(',') : default_value;
 }
 
-const public_paths = to_array(process.env.PEP_PROXY_PUBLIC_PATHS, [
-  '/iot/about',
-]);
+const public_paths = to_array(process.env.PEP_PROXY_PUBLIC_PATHS, ['/iot/about']);
 
 const options = {
   host: 'localhost',
   port: process.env.PEP_PROXY_PORT || config.port,
   timeout: 2000,
   method: 'GET',
-  path: public_paths[0] || '/',
+  path: public_paths[0] || '/'
 };
 
-const request = http.request(options, result => {
+const request = http.request(options, (result) => {
   // eslint-disable-next-line no-console
   console.info(`Performed health check, result ${result.statusCode}`);
   if (result.statusCode === http_code) {
@@ -30,11 +35,9 @@ const request = http.request(options, result => {
   }
 });
 
-request.on('error', err => {
+request.on('error', (err) => {
   // eslint-disable-next-line no-console
-  console.error(
-    `An error occurred while performing health check, error: ${err}`
-  );
+  console.error(`An error occurred while performing health check, error: ${err}`);
   process.exit(1);
 });
 
